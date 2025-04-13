5ee5a5096a96aaae115bd05f2001b6ee4140f482
Vladyslav Sheiko <vladyslav.sheiko@outlook.com>
2025-04-13 21:42:02 +0300

    Project Setup

---
+++ FluentCandleStick/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSpaStaticFiles(configuration => 
+{
+    configuration.RootPath = "wwwroot";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSpaStaticFiles();
+}
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToController("Index", "Fallback");
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+    
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+    }
+});
+
+app.Run();
